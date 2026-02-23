116af8164464d73153f25d960dc4eb290237d6a0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2026-02-24 00:53:29 +0600

    feat(prisma): integrate Prisma with NestJS and update dependencies
    - Added Prisma configuration to package.json.
    - Removed obsolete prisma.config.ts file.
    - Updated schema.prisma to use "prisma-client-js" and set DATABASE_URL.
    - Integrated PrismaModule into AppModule for dependency injection.
    - Created PrismaService to manage database connection lifecycle.
    - Updated dependencies for Prisma and related packages.

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    console.log('Database connecting...');
+    try {
+      await this.$connect();
+      console.log('Database connected successfully!');
+    } catch (error: any) {
+      console.log(error);
+      console.error(`Failed to connect to database. Error: ${error?.message}`);
+    }
+  }
+}
