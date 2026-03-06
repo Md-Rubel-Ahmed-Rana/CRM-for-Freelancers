6bee3ce33c57957c9080316d80f8d66b564f9cd0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2026-03-07 00:38:19 +0600

    feat(redux): Redux RTK Query setup completed

---
+++ frontend/src/redux/apiSlice.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = process.env.NEXT_PUBLIC_BASE_API as string;
+
+const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseApi,
+    credentials: "include",
+  }),
+  tagTypes: ["auth"],
+  endpoints: () => ({}),
+});
+
+export default apiSlice;
